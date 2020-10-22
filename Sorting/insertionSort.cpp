76bb9853e655dd165b72113897c56dcc533b8b6d
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-22 19:14:39 +0530

    add code in insertionSort.cpp

---
+++ Sorting/insertionSort.cpp
@@ -0,0 +1,39 @@
+/*  
+*   Created by IntelliJ IDEA.
+*   Author: Kshitij Varshney (kshitijvarshne1)
+*   Date: 22-Oct-20
+*   Time: 7:12 PM
+*   File: insertionSort.cpp
+*/
+# include<iostream>
+# include<climits>
+# include<algorithm>
+# include <iomanip>
+
+using namespace std;
+
+void insertion_sort(int a[], int n) {
+    for (int i = 1; i < n; i++) {
+        int e = a[i];
+        int j = i - 1;
+        while (j >= 0 and a[j] > e) {
+            a[j + 1] = a[j];
+            j = j - 1;
+        }
+        a[j + 1] = e;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    insertion_sort(a, n);
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
