5c3119d1bd4fe69070e83aab5beb69a9af8861e6
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-23 23:51:24 +0530

    add code to StringPermutation.cpp

--- Recursion-Backtracking/StringPermutation.cpp
+++ Recursion-Backtracking/StringPermutation.cpp
@@ -13,4 +13,34 @@
 # include<cstring>
 # include<iomanip>
 
+using namespace std;
+
+void stringPermutation(char in[], int i, set<string> &s) {
+    //Base case
+    if (in[i] == '\0') {
+        //cout << in << endl;
+        string t(in);
+        s.insert(t);
+        return;
+    }
+    //Recursive case
+    for (int j = i; in[j] != '\0'; j++) {
+        swap(in[i], in[j]);
+        stringPermutation(in, i + 1, s);
+        //Backtracking
+        swap(in[i], in[j]);
+    }
+    return;
+}
+
+int main() {
+    char in[100];
+    cin >> in;
+    set<string> s;
+    stringPermutation(in, 0, s);
+    for (auto str : s) {
+        cout << str << endl;
+    }
+    return 0;
+}
 
