a6206b295647f63362ce3e238d7fd634fd292588
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-24 21:34:32 +0530

    create NQueensCountOutcomes.cpp

---
+++ Recursion-Backtracking/NQueensCountOutcomes.cpp
@@ -0,0 +1,21 @@
+/*  
+*   Created by CLion IDE.
+*   Author: Kshitij Varshney (kshitijvarshne1)
+*   Date: 24-Oct-20
+*   Time: 9:34 PM
+*   File: NQueensCountOutcomes.cpp
+*/
+# include<iostream>
+# include<climits>
+# include<cmath>
+# include<algorithm>
+# include<bits/stdc++.h>
+# include<cstring>
+# include<iomanip>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
