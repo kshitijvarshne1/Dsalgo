416d9da1acd46ed3a1e92cca4a4598566d996219
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-25 21:28:46 +0530

    add code to WaveSort.cpp

--- Sorting/WaveSort.cpp
+++ Sorting/WaveSort.cpp
@@ -17,6 +17,21 @@
 using namespace std;
 
 int main() {
+    int a[] = {1, 3, 4, 2, 7, 8};
+    int n = sizeof(a) / sizeof(int);
+    for (int i = 0; i < n; i += 2) {
+        //Pre element
+        if (i != 0 and a[i] < a[i - 1]) {
+            swap(a[i], a[i - 1]);
+        }
+        //next element
+        if (i != (n - 1) and a[i] < a[i + 1]) {
+            swap(a[i], a[i + 1]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
     return 0;
 }
 
