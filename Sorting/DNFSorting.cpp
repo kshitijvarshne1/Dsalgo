6bf169e0a32dd210071144091f3ba94f64b3bb77
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-25 15:52:40 +0530

    add code to DNFSorting.cpp

--- Sorting/DNFSorting.cpp
+++ Sorting/DNFSorting.cpp
@@ -16,7 +16,34 @@
 
 using namespace std;
 
+void dnfSort(int a[], int n) {
+    //DNF:- Dutch National Flag
+    //Sort array of 0s 1, 2s
+    int lo = 0;
+    int hi = n - 1;
+    int mid = 0;
+    while (mid <= hi) {
+        if (a[mid] == 0) {
+            swap(a[mid], a[lo]);
+            lo++;
+            mid++;
+        } else if (a[mid] == 1) {
+            mid++;
+        } else if (a[mid] == 2) {
+            swap(a[mid], a[hi]);
+            hi--;
+        }
+    }
+    return;
+}
+
 int main() {
+    int a[] = {1, 0, 1, 1, 2, 2, 2, 0, 0, 1, 0, 1, 2};
+    int n = sizeof(a) / sizeof(int);
+    dnfSort(a, n);
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
     return 0;
 }
 
