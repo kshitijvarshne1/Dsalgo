013b2b40e4690b458f89db9559bfe45fafd95d6d
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-25 13:23:16 +0530

    add code to BucketSort.cpp

--- Sorting/BucketSort.cpp
+++ Sorting/BucketSort.cpp
@@ -16,7 +16,38 @@
 
 using namespace std;
 
+class Student {
+public:
+    int marks;
+    string name;
+};
+
+void bucketSort(Student s[], int n) {
+    //Assuming marks are in range 0-100
+    vector<Student> v[101];
+
+    //O(N)
+    for (int i = 0; i < n; i++) {
+        int m = s[i].marks;
+        v[m].push_back(s[i]);
+    }
+    //iterate over the vector and the print the students
+    for (int i = 100; i >= 0; i--) {
+        for (vector<Student>::iterator it = v[i].begin(); it != v[i].end(); it++) {
+            cout << (*it).marks << " " << (*it).name << endl;
+        }
+    }
+
+}
+
 int main() {
+    Student s[1000];
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s[i].marks >> s[i].name;
+    }
+    bucketSort(s, n);
     return 0;
 }
 
