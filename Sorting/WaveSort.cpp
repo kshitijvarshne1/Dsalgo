8372f79c6a637c6a13d507c013be096a13dcbf2b
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-25 21:28:22 +0530

    create WaveSort.cpp

---
+++ Sorting/WaveSort.cpp
@@ -0,0 +1,22 @@
+/*  
+*   Created by CLion IDE.
+*   Author: Kshitij Varshney (kshitijvarshne1)
+*   Date: 25-Oct-20
+*   Time: 9:27 PM
+*   File: WaveSort.cpp
+*/
+# include<iostream>
+# include<climits>
+# include<cmath>
+# include<algorithm>
+# include<bits/stdc++.h>
+# include<cstring>
+# include<iomanip>
+# include<vector>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
