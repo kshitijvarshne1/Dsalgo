2ef74d21cb125e23b1a023c9f83dc2e28a384043
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-26 18:56:19 +0530

    create FastPower.cpp

---
+++ Basic/FastPower.cpp
@@ -0,0 +1,22 @@
+/*  
+*   Created by CLion IDE.
+*   Author: Kshitij Varshney (kshitijvarshne1)
+*   Date: 26-Oct-20
+*   Time: 6:55 PM
+*   File: FastPower.cpp
+*/
+# include<iostream>
+# include<climits>
+# include<cmath>
+# include<algorithm>
+# include<bits/stdc++.h>
+# include<cstring>
+# include<iomanip>
+# include<vector>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
