560ecbdec97f9e0099f0b375428d4d38f63b0caa
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-26 18:56:42 +0530

    add code to FastPower.cpp

--- Basic/FastPower.cpp
+++ Basic/FastPower.cpp
@@ -16,7 +16,23 @@
 
 using namespace std;
 
+int fastPower(int a, int n) {
+    int ans = 1;
+    while (n > 0) {
+        int last_bit = (n & 1);
+        if (last_bit) {
+            ans = ans * a;
+        }
+        a = a * a;
+        n = n >> 1;
+    }
+    return ans;
+}
+
 int main() {
+    long long int a, n;
+    cin >> a >> n;
+    cout << fastPower(a, n);
     return 0;
 }
 
