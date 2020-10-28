20b8a46de2fb88f9e503a54f9a6b7994019e31e9
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-28 10:52:47 +0530

    add code to Kadane'sAlgo(O(N^2)).cpp

--- Basic/Kadane'sAlgo(O(N^2)).cpp
+++ Basic/Kadane'sAlgo(O(N^2)).cpp
@@ -17,6 +17,31 @@
 using namespace std;
 
 int main() {
+    int n, a[1000];
+    int maxSum = 0;
+    int left = -1;
+    int right = -1;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int currentSum = 0;
+            for (int k = i; k <= j; k++) {
+                currentSum += a[k];
+            }
+            if (maxSum < currentSum) {
+                maxSum = currentSum;
+                left = i;
+                right = j;
+            }
+        }
+    }
+    cout << "Max Sum:- " << maxSum << endl;
+    for (int i = left; i <= right; i++) {
+        cout << a[i] << ", ";
+    }
     return 0;
 }
 
