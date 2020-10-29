851f4b1833b41c5a8adda6432129b3e8d12ff4ae
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-29 16:21:50 +0530

    create TrailingZeroes.cpp

---
+++ Basic/TrailingZeroes.cpp
@@ -0,0 +1,22 @@
+/*  
+*   Created by CLion IDE.
+*   Author: Kshitij Varshney (kshitijvarshne1)
+*   Date: 29-Oct-20
+*   Time: 4:21 PM
+*   File: TrailingZeroes.cpp
+*/
+# include<iostream>
+# include<climits>
+# include<cmath>
+# include<algorithm>
+# include<bits/stdc++.h>
+# include<cstring>
+# include<iomanip>
+# include<vector>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
