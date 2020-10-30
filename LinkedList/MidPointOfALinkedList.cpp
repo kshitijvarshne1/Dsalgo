c23ae69e25b7eb71c7e6c16bd8efd01232311f8f
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-30 17:44:20 +0530

    add code to MidPointOfALinkedList.cpp

--- LinkedList/MidPointOfALinkedList.cpp
+++ LinkedList/MidPointOfALinkedList.cpp
@@ -16,7 +16,61 @@
 
 using namespace std;
 
+class node {
+public:
+    int data;
+    node *next;
+
+    node(int d) {
+        data = d;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int d) {
+    if (head == NULL) {
+        head = new node(d);
+        return;
+    }
+    node *n = new node(d);
+    n->next = head;
+    head = n;
+}
+
+node *midpoint(node *head) {
+    if (head == NULL or head->next == NULL) {
+        return head;
+    }
+    node *slow = head;
+    node *fast = head;
+
+    while (fast != NULL and fast->next != NULL) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    return slow;
+}
+
+void print(node *head) {
+    while (head != NULL) {
+        cout << head->data << "->";
+        head = head->next;
+    }
+}
+
+
 int main() {
+    node *head = NULL;
+    int n;
+    cin >> n;
+    int a;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        insertAtHead(head, a);
+    }
+    node *mid = midpoint(head);
+    cout << mid->data;
+
     return 0;
 }
 
