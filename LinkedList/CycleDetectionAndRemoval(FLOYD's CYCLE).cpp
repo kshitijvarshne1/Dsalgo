794d6538b221d79d10ecd34d076533cd08206935
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-30 23:48:40 +0530

    add code to CycleDetectionAndRemoval(FLOYD's CYCLE).cpp

--- LinkedList/CycleDetectionAndRemoval(FLOYD's CYCLE).cpp
+++ LinkedList/CycleDetectionAndRemoval(FLOYD's CYCLE).cpp
@@ -5,6 +5,38 @@
 *   Time: 11:46 PM
 *   File: CycleDetectionAndRemoval.cpp
 */
+
+
+// head - Head pointer of the Linked List
+// Return a boolean value indicating the presence of cycle
+// If the cycle is present, modify the linked list to remove the cycle as well
+
+/*
+bool floydCycleRemoval(Node *head)
+{
+    Node* slow = head;
+    Node* fast =head;
+    while( fast!=NULL && fast->next!=NULL){
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast){
+        //for removal of the connection
+            slow = head;
+            while (slow->next != fast->next) {
+                slow = slow->next;
+                fast = fast->next;
+            }
+            fast->next = NULL;
+            return true;
+        }
+    }
+    return false;
+}
+
+
+*/
+
+
 # include<iostream>
 # include<climits>
 # include<cmath>
