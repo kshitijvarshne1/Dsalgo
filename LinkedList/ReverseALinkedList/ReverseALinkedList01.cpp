45c8b5310caa2dc7d6ce06077d3ea51ab39f7c7c
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-30 17:04:12 +0530

    add code to ReverseALinkedList01.cpp

--- LinkedList/ReverseALinkedList/ReverseALinkedList01.cpp
+++ LinkedList/ReverseALinkedList/ReverseALinkedList01.cpp
@@ -16,7 +16,63 @@
 
 using namespace std;
 
+class node {
+public:
+    int data;
+    node *next;
+
+    node(int d) {
+        data = d;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int d) {
+    if (head == NULL) {
+        head = new node(d);
+        return;
+    }
+    node *n = new node(d);
+    n->next = head;
+    head = n;
+}
+
+void reverseLinkedList(node *&head) {
+    node *c = head;
+    node *p = NULL;
+    node *n;
+    while (c != NULL) {
+        //save the next node
+        n = c->next;
+        c->next = p;
+        //update p and c take them 1 step forward
+        p = c;
+        c = n;
+    }
+    head = p;
+
+}
+
+void print(node *head) {
+    while (head != NULL) {
+        cout << head->data << "->";
+        head = head->next;
+    }
+}
+
 int main() {
+    node *head = NULL;
+    int n;
+    cin >> n;
+    int a;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        insertAtHead(head, a);
+    }
+    reverseLinkedList(head);
+
+    print(head);
+
     return 0;
 }
 
