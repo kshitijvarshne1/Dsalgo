83bbcca5b4e7fba11c756a53c16516cf69b320cb
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-30 17:22:47 +0530

    add code to RecursivelyReverseALinkedList(OPTIMISED).cpp

--- LinkedList/ReverseALinkedList/RecursivelyReverseALinkedList(OPTIMISED).cpp
+++ LinkedList/ReverseALinkedList/RecursivelyReverseALinkedList(OPTIMISED).cpp
@@ -2,7 +2,7 @@
 *   Created by CLion IDE.
 *   Author: Kshitij Varshney (kshitijvarshne1)
 *   Date: 30-Oct-20
-*   Time: 5:21 PM
+*   Time: 5:20 PM
 *   File: RecusivelyReverseALinkedList.cpp
 */
 # include<iostream>
@@ -16,7 +16,63 @@
 
 using namespace std;
 
+class node {
+public:
+    int data;
+    node *next;
+
+    node(int d) {
+        data = d;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int d) {
+    if (head == NULL) {
+        head = new node(d);
+        return;
+    }
+    node *n = new node(d);
+    n->next = head;
+    head = n;
+}
+
+node *recursivelyReverse(node *head) {
+    //smallest linked list
+    if (head->next == NULL or head == NULL) {
+        return head;
+    }
+    //Recursive case
+    node *shead = recursivelyReverse(head->next);
+
+    node *temp = head->next;
+    temp->next = head;
+    head->next = NULL;
+    return shead;
+
+}
+
+void print(node *head) {
+    while (head != NULL) {
+        cout << head->data << "->";
+        head = head->next;
+    }
+}
+
+
 int main() {
+    node *head = NULL;
+    int n;
+    cin >> n;
+    int a;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        insertAtHead(head, a);
+    }
+    head = recursivelyReverse(head);
+    print(head);
+
     return 0;
 }
+
 
