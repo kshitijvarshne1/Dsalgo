3a01c53a0c9ab2fbdbab544934133be0ab8b1f6f
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-30 17:14:30 +0530

    add code to RecursivelyReverseALinkedList.cpp

--- LinkedList/ReverseALinkedList/RecursivelyReverseALinkedList.cpp
+++ LinkedList/ReverseALinkedList/RecursivelyReverseALinkedList.cpp
@@ -16,7 +16,65 @@
 
 using namespace std;
 
+class node {
+public:
+    int data;
+    node *next;
+
+    node(int d) {
+        data = d;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int d) {
+    if (head == NULL) {
+        head = new node(d);
+        return;
+    }
+    node *n = new node(d);
+    n->next = head;
+    head = n;
+}
+
+node *recursivelyReverse(node *head) {
+    //smallest linked list
+    if (head->next == NULL or head == NULL) {
+        return head;
+    }
+    //Recursive case
+    node *shead = recursivelyReverse(head->next);
+
+    node *temp = shead;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    head->next = NULL;
+    temp->next = head;
+    return shead;
+
+}
+
+void print(node *head) {
+    while (head != NULL) {
+        cout << head->data << "->";
+        head = head->next;
+    }
+}
+
+
 int main() {
+    node *head = NULL;
+    int n;
+    cin >> n;
+    int a;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        insertAtHead(head, a);
+    }
+    head = recursivelyReverse(head);
+    print(head);
+
     return 0;
 }
 
