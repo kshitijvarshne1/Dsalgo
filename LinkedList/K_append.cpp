ef9823d04ba38dbbb9e88030c46b580effed0526
kshitij varshney <kshitijvarshney92@gmail.com>
2020-10-31 20:34:43 +0530

    create K_append.cpp

---
+++ LinkedList/K_append.cpp
@@ -0,0 +1,22 @@
+rea/*
+*   Created by CLion IDE.
+*   Author: Kshitij Varshney (kshitijvarshne1)
+*   Date: 31-Oct-20
+*   Time: 8:34 PM
+*   File: K_append.cpp
+*/
+# include<iostream>
+# include<climits>
+# include<cmath>
+# include<algorithm>
+# include<bits/stdc++.h>
+# include<cstring>
+# include<iomanip>
+# include<vector>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
