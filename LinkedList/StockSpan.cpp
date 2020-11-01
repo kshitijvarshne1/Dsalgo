59e18d5059447de9438f8aeaeac379c1259755ca
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-02 00:26:05 +0530

    add code to StockSpan.cpp

--- LinkedList/StockSpan.cpp
+++ LinkedList/StockSpan.cpp
@@ -17,7 +17,44 @@
 
 using namespace std;
 
+void stockSpan(int prices[], int n, int span[]) {
+    stack<int> s;
+    s.push(0);
+    span[0] = 1;
+    //loop for the rest element
+    for (int i = 0; i < n; i++) {
+        int currentPrice = prices[i];
+        //topmost element that is higher than the current prices
+        while (!s.empty() and prices[s.top()] <= currentPrice) {
+            s.pop();
+        }
+        if (!s.empty()) {
+            int prev_highest = s.top();
+            span[i] = i - prev_highest;
+        } else {
+            span[i] = i + 1;
+        }
+        //push this element into the stack
+        s.push(i);
+    }
+}
+
 int main() {
+    int n;
+    cin >> n;
+    int prices[1000];
+    for (int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+    int span[1000] = {0};
+    stockSpan(prices, n, span);
+    for (int i = 0; i < n; i++) {
+        cout << span[i] << " ";
+    }
+    cout << "END";
+
+
     return 0;
 }
+
 
