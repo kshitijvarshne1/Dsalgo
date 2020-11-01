54d9681305a86fb980f28c25b56ba48c812fb49f
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-01 12:18:29 +0530

    add code to IsPallindromicLinkedList.cpp

--- LinkedList/IsPallindromicLinkedList.cpp
+++ LinkedList/IsPallindromicLinkedList.cpp
@@ -16,7 +16,90 @@
 
 using namespace std;
 
-int main() {
-    return 0;
+class node {
+public:
+    int data;
+    node *next;
+
+    //Constructor
+    node(int d) {
+        data = d;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node *&head, int data) {
+    if (head == NULL) {
+        head = new node(data);
+        return;
+    }
+    node *tail = head;
+    while (tail->next != NULL) {
+        tail = tail->next;
+    }
+    tail->next = new node(data);
 }
 
+void take_input(node *&head) {
+    int n;
+    cin >> n;
+    while (n--) {
+        int data;
+        cin >> data;
+        insertAtTail(head, data);
+    }
+    return;
+}
+
+void print(node *head) {
+    if (head == NULL) {
+        return;
+    }
+    cout << head->data << " ";
+    print(head->next);
+}
+
+bool pallindromeCheck(node *head) {
+    node *slow = head;
+    node *fast = head;
+    node *mid = NULL;
+    while (fast != NULL and fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    //odd->fast is not null
+    if (fast != NULL) {
+        mid = slow->next;
+    } else {
+        mid = slow;
+    }
+    node *previous = NULL;
+    node *next = NULL;
+
+    while (mid != NULL) {
+        next = mid->next;
+        mid->next = previous;
+        previous = mid;
+        mid = next;
+    }
+    while (previous != NULL) {
+        if (previous->data != head->data) {
+            return false;
+        }
+        previous = previous->next;
+        head = head->next;
+    }
+    return true;
+
+}
+
+int main() {
+    int k;
+    node *head = NULL;
+    take_input(head);
+    if (pallindromeCheck(head)) {
+        cout << "true";
+    } else {
+        cout << "false";
+    }
+}
