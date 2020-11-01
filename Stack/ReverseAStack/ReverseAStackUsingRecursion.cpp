d2940627114b4a9a95406e9dbb81dfe88ddcf378
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-01 17:47:43 +0530

    add code to ReverseAStackUsingRecursion.cpp

--- Stack/ReverseAStack/ReverseAStackUsingRecursion.cpp
+++ Stack/ReverseAStack/ReverseAStackUsingRecursion.cpp
@@ -17,7 +17,51 @@
 
 using namespace std;
 
+void insertAtBottom(stack<int> &s, int x) {
+    //base case
+    if (s.empty()) {
+        s.push(x);
+        return;
+    }
+    //recursive case
+    int data = s.top();
+    s.pop();
+    insertAtBottom(s, x);
+    s.push(data);
+
+}
+
+
+void reverseStack(stack<int> &s) {
+    //base case
+    if (s.empty()) {
+        return;
+    }
+    //otherwise
+    //pop out the top element and insert at the bottom of reversed smaller stack
+    int x = s.top();
+    s.pop();
+    //recursively reverse the smaller stack
+    reverseStack(s);
+    insertAtBottom(s, x);
+}
+
 int main() {
+    stack<int> s;
+    int n;
+    cin >> n;
+    int k;
+    for (int i = 0; i < n; i++) {
+        cin >> k;
+        s.push(k);
+    }
+    reverseStack(s);
+    while (!s.empty()) {
+        cout << s.top() << endl;
+        s.pop();
+    }
+
     return 0;
 }
+
 
