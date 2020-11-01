b7c88178e84a5cf2493f4c32fb18fbc59ea5f02f
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-01 16:57:33 +0530

    add code to ReverseAStackUsingAnotherStack.cpp

--- Stack/ReverseAStack/ReverseAStackUsingAnotherStack.cpp
+++ Stack/ReverseAStack/ReverseAStackUsingAnotherStack.cpp
@@ -17,7 +17,46 @@
 
 using namespace std;
 
+void transfer(stack<int> &s1, stack<int> &s2, int n) {
+    for (int i = 0; i < n; i++) {
+        s2.push(s1.top());
+        s1.pop();
+    }
+}
+
+void reverseStack(stack<int> &s1) {
+    //helper stack
+    stack<int> s2;
+    int n = s1.size();
+    for (int i = 0; i < n; i++) {
+        //pick the element at top & insert it at the bottom
+        int x = s1.top();
+        s1.pop();
+        //transfer n-i-1 element from stack s1 into s2
+        transfer(s1, s2, n - i - 1);
+        //insert the element x in s1
+        s1.push(x);
+        //transfer n-i-1 element from stack s2 into s1
+        transfer(s2, s1, n - i - 1);
+    }
+}
+
 int main() {
+    stack<int> s;
+    int n;
+    cin >> n;
+    int k;
+    for (int i = 0; i < n; i++) {
+        cin >> k;
+        s.push(k);
+    }
+    reverseStack(s);
+    while (!s.empty()) {
+        cout << s.top() << endl;
+        s.pop();
+    }
+
     return 0;
 }
+
 
