4e49e004dd25246099e743eede3c4e76d86ab869
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-01 19:57:32 +0530

    add code to BalancedParenthesisForSquareBrackets,ParenthesisAndBraces.cpp

--- Stack/BalancedParenthesis/BalancedParenthesisForSquareBrackets,ParenthesisAndBraces.cpp
+++ Stack/BalancedParenthesis/BalancedParenthesisForSquareBrackets,ParenthesisAndBraces.cpp
@@ -17,7 +17,54 @@
 
 using namespace std;
 
+bool isValidExpression(char *str) {
+    stack<char> s;
+    char popch;
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+        if (ch == '(' or ch == '{' or ch == '[') {
+            s.push(ch);
+        } else if (ch == ')' or ch == '}' or ch == ']') {
+            if (s.empty()) {
+                return false;
+            } else {
+                switch (ch) {
+                    case ')':    //for closing parenthesis, pop it and check for braces and square brackets
+                        popch = s.top();
+                        s.pop();
+                        if (popch == '{' || popch == '[')
+                            return false;
+                        break;
+                    case '}': //for closing braces, pop it and check for parenthesis and square brackets
+                        popch = s.top();
+                        s.pop();
+                        if (popch == '(' || popch == '[')
+                            return false;
+                        break;
+                    case ']': //for closing square bracket, pop it and check for braces and parenthesis
+                        popch = s.top();
+                        s.pop();
+                        if (popch == '(' || popch == '{')
+                            return false;
+                        break;
+                }
+            }
+
+        }
+    }
+    return s.empty();
+}
+
 int main() {
+    // This program for square brackets , parenthesis and braces
+    char s[100];
+    cin >> s;
+    if (isValidExpression(s)) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
+
     return 0;
 }
 
