4aa56b0f4dd553df524de07f9911a439be58829b
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-01 12:18:06 +0530

    create IsPallindromicLinkedList.cpp

---
+++ LinkedList/IsPallindromicLinkedList.cpp
@@ -0,0 +1,22 @@
+/*  
+*   Created by CLion IDE.
+*   Author: Kshitij Varshney (kshitijvarshne1)
+*   Date: 01-Nov-20
+*   Time: 12:17 PM
+*   File: IsPallindromicLinkedList.cpp
+*/
+# include<iostream>
+# include<climits>
+# include<cmath>
+# include<algorithm>
+# include<bits/stdc++.h>
+# include<cstring>
+# include<iomanip>
+# include<vector>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
