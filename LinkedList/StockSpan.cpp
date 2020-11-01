689c2bf8f5940669dfac613862f8546b7887bc51
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-02 00:25:09 +0530

    create StockSpan.cpp

---
+++ LinkedList/StockSpan.cpp
@@ -0,0 +1,23 @@
+/*  
+*   Created by CLion IDE.
+*   Author: Kshitij Varshney (kshitijvarshne1)
+*   Date: 02-Nov-20
+*   Time: 12:24 AM
+*   File: StockSpan.cpp
+*/
+# include<iostream>
+# include<climits>
+# include<cmath>
+# include<algorithm>
+# include<bits/stdc++.h>
+# include<cstring>
+# include<iomanip>
+# include<vector>
+# include<stack>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
