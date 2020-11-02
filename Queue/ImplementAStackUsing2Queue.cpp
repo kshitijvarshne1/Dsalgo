13942a3fb2a273e6f4402361e70fd3a70a9c22e4
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-03 00:03:24 +0530

    create ImplementAStackUsing2Queue.cpp

---
+++ Queue/ImplementAStackUsing2Queue.cpp
@@ -0,0 +1,24 @@
+/*  
+*   Created by CLion IDE.
+*   Author: Kshitij Varshney (kshitijvarshne1)
+*   Date: 03-Nov-20
+*   Time: 12:03 AM
+*   File: ImplementAStackUsing2Queue.cpp
+*/
+# include<iostream>
+# include<climits>
+# include<cmath>
+# include<algorithm>
+# include<bits/stdc++.h>
+# include<cstring>
+# include<iomanip>
+# include<vector>
+# include<stack>
+# include<queue>
+
+using namespace std;
+
+int main() {
+    return 0;
+}
+
