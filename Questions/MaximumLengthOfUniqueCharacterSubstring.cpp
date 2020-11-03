b2542e523ad76c119e7006f11c7ed91fd59c0eaf
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-03 12:19:32 +0530

    add code to MaximumLengthOfUniqueCharacterSubstring.cpp

--- Questions/MaximumLengthOfUniqueCharacterSubstring.cpp
+++ Questions/MaximumLengthOfUniqueCharacterSubstring.cpp
@@ -17,8 +17,47 @@
 # include<queue>
 
 using namespace std;
-
+//Maximum length of unique length substring
 int main() {
+    char a[10000];
+    cin >> a;
+
+    int n = strlen(a);
+    int current_len = 1;
+    int max_len = 1;
+
+    int visited[256];
+
+    for (int i = 0; i < 256; i++) {
+        visited[i] = -1;
+    }
+
+
+    visited[a[0]] = 0;
+
+    for (int i = 1; i < n; i++) {
+        int last_occ = visited[a[i]];
+
+        //Expansion
+        if (last_occ == -1 || i - current_len > last_occ) {
+            current_len += 1;
+        }
+            //Expansion + Contraction
+        else {
+            if (current_len > max_len) {
+                max_len = current_len;
+            }
+            current_len = i - last_occ;
+        }
+        visited[a[i]] = i;
+    }
+
+    if (current_len > max_len) {
+        max_len = current_len;
+    }
+    cout << max_len << endl;
+
+
     return 0;
 }
 
