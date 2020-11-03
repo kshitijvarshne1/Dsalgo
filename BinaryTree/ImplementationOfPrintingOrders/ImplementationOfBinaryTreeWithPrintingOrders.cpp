ff201c61c2b1a6ee7137f2d540f34ad8c30ad6d0
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-03 21:19:20 +0530

    add code to ImplementationOfBinaryTreeWithPrintingOrders.cpp

--- BinaryTree/ImplementationOfPrintingOrders/ImplementationOfBinaryTreeWithPrintingOrders.cpp
+++ BinaryTree/ImplementationOfPrintingOrders/ImplementationOfBinaryTreeWithPrintingOrders.cpp
@@ -18,7 +18,79 @@
 
 using namespace std;
 
+class node {
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d) {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+
+};
+
+node *buildTree() {
+    int d;
+    cin >> d;
+
+    if (d == -1) {
+        return NULL;
+    }
+    node *root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+void printPreOrder(node *root) {
+    if (root == NULL) {
+        return;
+    }
+    //otherwise print root first followed by subtree(children)
+    cout << root->data << " ";
+    printPreOrder(root->left);
+    printPreOrder(root->right);
+}
+
+void printInOrder(node *root) {
+    if (root == NULL) {
+        return;
+    }
+    //otherwise left root right
+    printInOrder(root->left);
+    cout << root->data << " ";
+    printInOrder(root->right);
+}
+
+void printPostOrder(node *root) {
+    if (root == NULL) {
+        return;
+    }
+    //otherwise left root right
+    printPostOrder(root->left);
+    printPostOrder(root->right);
+    cout << root->data << " ";
+}
+
 int main() {
+    node *root = buildTree();
+    printPreOrder(root);
+    cout << endl;
+    printInOrder(root);
+    cout << endl;
+    printPostOrder(root);
     return 0;
 }
 
+//
+/* Input:-
+ * 8 10 1 -1 -1 6 9 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
+ * output:-
+ *
+ *  Pre order  8 10 1 6 9 7 3 14 13
+ *  In order   1 10 9 6 7 8 3 13 14
+ *  Post Order 1 9 7 6 10 13 14 3 8
+ * */
