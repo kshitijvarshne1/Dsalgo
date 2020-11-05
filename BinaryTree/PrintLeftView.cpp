de38173b870d58059891219cfa3faabb1c386c03
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-05 18:12:01 +0530

    add code to PrintLeftView.cpp

--- BinaryTree/PrintLeftView.cpp
+++ BinaryTree/PrintLeftView.cpp
@@ -18,7 +18,71 @@
 
 using namespace std;
 
+class node {
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d) {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+
+};
+
+node *levelOrderBuild() {
+    int d;
+    cin >> d;
+    node *root = new node(d);
+    queue<node *> q;
+    q.push(root);
+
+    while (!q.empty()) {
+        node *n = q.front();
+        q.pop();
+        int c1, c2;
+        cin >> c1 >> c2;
+        if (c1 != -1) {
+            n->left = new node(c1);
+            q.push(n->left);
+        }
+        if (c2 != -1) {
+            n->right = new node(c2);
+            q.push(n->right);
+        }
+    }
+    return root;
+}
+
+void printRightView(node *root, int level, int &maxlevel) {
+    if (root == NULL) {
+        return;
+    }
+    if (maxlevel < level) {
+        //have discovered a new level
+        cout << root->data << " ";
+        maxlevel = level;
+    }
+    // left
+    printRightView(root->left, level + 1, maxlevel);
+    //right
+    printRightView(root->right, level + 1, maxlevel);
+
+}
+
 int main() {
+    node *root = levelOrderBuild();
+    int maxlevel = -1;
+    printRightView(root, 0, maxlevel);
     return 0;
 }
+/* Input:-
+ * 1 2 3 4 5 -1 6 -1 -1 -1 -1 -1 -1
+ *
+ * output:-
+ * 1 2 4
+ * */
+
 
