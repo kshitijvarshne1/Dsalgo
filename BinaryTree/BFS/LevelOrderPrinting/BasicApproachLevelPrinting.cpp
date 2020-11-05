a5f4ef619664d9927e7046e0f374bee7f88e881b
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-05 18:01:24 +0530

    add code to BasicApproachLevelPrinting.cpp

--- BinaryTree/BFS/LevelOrderPrinting/BasicApproachLevelPrinting.cpp
+++ BinaryTree/BFS/LevelOrderPrinting/BasicApproachLevelPrinting.cpp
@@ -16,9 +16,91 @@
 # include<stack>
 # include<queue>
 
+# include<iostream>
+# include<climits>
+# include<cmath>
+# include<algorithm>
+# include<bits/stdc++.h>
+# include<cstring>
+# include<iomanip>
+# include<vector>
+# include<stack>
+# include<queue>
+
 using namespace std;
 
+class node {
+public:
+    int data;
+    node *left;
+    node *right;
+
+    node(int d) {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+
+};
+
+node *buildTree() {
+    int d;
+    cin >> d;
+    if (d == -1) {
+        return NULL;
+    }
+    node *root = new node(d);
+    root->left = buildTree();
+    root->right = buildTree();
+    return root;
+}
+
+int height(node *root) {
+    if (root == NULL) {
+        return 0;
+    }
+    int ls = height(root->left);
+    int rs = height(root->right);
+
+    return max(ls, rs) + 1;
+}
+
+void printKLevel(node *root, int k) {
+    if (root == NULL) {
+        return;
+    }
+    if (k == 1) {
+        cout << root->data << " ";
+        return;
+    }
+    printKLevel(root->left, k - 1);
+    printKLevel(root->right, k - 1);
+    return;
+
+}
+
+void printAllLevels(node *root) {
+    int h = height(root);
+    for (int i = 1; i <= h; i++) {
+        printKLevel(root, i);
+        cout << endl;
+    }
+    return;
+}
+
+
 int main() {
+    node *root = buildTree();
+    printAllLevels(root);
     return 0;
 }
+/* Input:-
+ * 8 10 1 -1 -1 6 9 -1 -1 7 -1 -1 3 -1 14 13 -1 -1 -1
+ *
+ * output:-
+ * 8
+ * 10 3
+ * 1 6 14
+ * 9 7 13
+ * */
 
