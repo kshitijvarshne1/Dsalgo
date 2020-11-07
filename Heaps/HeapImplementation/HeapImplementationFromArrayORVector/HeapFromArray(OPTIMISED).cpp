5446850f03a9e9b39257fd8f407b43df619e1c2e
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-07 20:42:38 +0530

    add code to HeapFromArray(OPTIMISED).cpp

--- Heaps/HeapImplementation/HeapImplementationFromArrayORVector/HeapFromArray(OPTIMISED).cpp
+++ Heaps/HeapImplementation/HeapImplementationFromArrayORVector/HeapFromArray(OPTIMISED).cpp
@@ -18,7 +18,31 @@
 
 using namespace std;
 
-int main() {
-    return 0;
+//Time Complexity-> O(N)
+void buildHeapOptimised(vector<int> &v) {
+    for (int i = v.size() - 1; i >= 1; i--) {
+        int idx = i;
+        int parent = (i - 1) / 2;
+        while (idx > 0 and v[idx] > v[parent]) {    //for maxheap -> v[idx]>v[parent]   /for minheap -> v[idx]<v[parent]
+            swap(v[idx], v[parent]);
+            idx = parent;
+            parent = (parent - 1) / 2;
+        }
+    }
 }
 
+
+int main() {
+    vector<int> v{10, 20, 5, 6, 1, 8, 9, 4};
+    buildHeapOptimised(v);
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    return 0;
+}
+/*
+Input:-
+10, 20, 5, 6, 1, 8, 9, 4
+Output:-
+20 10 9 6 1 5 8 4
+ */
