421e7d2f6ba308f16e1b50c3b0d5f31bb18c9b93
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-08 19:31:11 +0530

    add code to KClosestNumber.cpp

--- Heaps/Priority_Queue/KClosestNumber.cpp
+++ Heaps/Priority_Queue/KClosestNumber.cpp
@@ -17,8 +17,26 @@ c/*
 # include<queue>
 
 using namespace std;
+//k Closest Numbers
 
 int main() {
-    return 0;
+    priority_queue<pair<int, int>> pq; //By default priority_queue like as maxHeap //for minHeap we use  priority_queue<int,vector<int>,greater<int>>pq;
+    pair<int, int> temp;
+    int n;
+    cin >> n;
+    int x, k;
+    cin >> x >> k;
+    for (int j = 0; j < n; j++) {
+        int num;
+        cin >> num;
+        temp = make_pair(abs(num - x), num);
+        pq.push(temp);
+        if (pq.size() > k) {
+            pq.pop();
+        }
+    }
+    while (pq.size() > 0) {
+        cout << pq.top().second << " ";
+        pq.pop();
+    }
 }
-
