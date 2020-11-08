c8047073c1af160443483849ae05f1905ef42d58
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-08 23:24:15 +0530

    add code to SortingAccToTheirFrequency.cpp

--- Heaps/Priority_Queue/SortingAccToTheirFrequency.cpp
+++ Heaps/Priority_Queue/SortingAccToTheirFrequency.cpp
@@ -17,8 +17,38 @@
 # include<queue>
 
 using namespace std;
+//Sorting according to their frequency
+void SortingAccToTheirFrequency(priority_queue<pair<int, int>> pq, int arr[], int n) {
+    unordered_map<int, int> mp;//use to count frequency //hash table
+
+    for (int i = 0; i < n; i++) {
+        mp[arr[i]]++;
+    }
+
+    for (auto x : mp) {
+        pq.push(make_pair(x.second, x.first));
+    }
+    while (pq.size() > 0) {
+        int fre = pq.top().first;
+        int ele = pq.top().second;
+
+        while (fre--) {
+            cout << ele << " ";
+        }
+        pq.pop();
+    }
+
+}
 
 int main() {
+    priority_queue<pair<int, int>> pq;
+    int n;
+    cin >> n;
+    int a[n];
+    for (int j = 0; j < n; j++) {
+        cin >> a[j];
+    }
+    SortingAccToTheirFrequency(pq, a, n);
     return 0;
 }
 
