12ae9aef734c6bf0ce3c4abb275de579b16b9e5f
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-09 00:49:52 +0530

    add code to KthClosestToOrigin.cpp

--- Heaps/Priority_Queue/KthClosestToOrigin.cpp
+++ Heaps/Priority_Queue/KthClosestToOrigin.cpp
@@ -17,8 +17,33 @@
 # include<queue>
 
 using namespace std;
+//find kth closest to origin
+void kthClosestToOrigin(int a[100][100], int n, int k) {
+    priority_queue<pair<int, pair<int, int>>> pq;
 
-int main() {
-    return 0;
+    for (int i = 0; i < n; i++) {
+        int x = a[i][0];
+        int y = a[i][1];
+        int dis = x * x + y * y;
+        pq.push(make_pair(dis, make_pair(x, y)));
+        if (pq.size() > k) {
+            pq.pop();
+        }
+    }
+    cout << pq.top().second.first << " " << pq.top().second.second;
 }
 
+int main() {
+
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    int a[100][100];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2; j++) {
+            cin >> a[i][j];
+        }
+    }
+    kthClosestToOrigin(a, n, k);
+}
