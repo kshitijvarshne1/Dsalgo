2752e3ff03c6fd1052598f71561616399454aa12
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-09 00:13:36 +0530

    add code to kthSmallestElement.cpp

--- Heaps/Priority_Queue/kthSmallestElement.cpp
+++ Heaps/Priority_Queue/kthSmallestElement.cpp
@@ -17,8 +17,27 @@
 # include<queue>
 
 using namespace std;
-
-int main() {
-    return 0;
+//find kth smallest element from the array
+int kthSmallestElement(int a[], int n, int k) {
+    priority_queue<int> pq; //maxHeap
+    for (int i = 0; i < n; i++) {
+        pq.push(a[i]);
+        if (pq.size() > k) {
+            pq.pop();
+        }
+    }
+    return pq.top();
 }
 
+int main() {
+
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << kthSmallestElement(a, n, k);
+}
