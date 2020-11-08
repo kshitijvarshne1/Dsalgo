6a8cc9517cf246823cc506153e6fb96b3ea7e0ec
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-08 23:57:52 +0530

    add code to JoinTheRopes.cpp

--- Heaps/Priority_Queue/JoinTheRopes.cpp
+++ Heaps/Priority_Queue/JoinTheRopes.cpp
@@ -16,9 +16,33 @@
 # include<stack>
 # include<queue>
 
+
+//Join the ropes
 using namespace std;
 
-int main() {
-    return 0;
+int joinTheRopes(priority_queue<int, vector<int>, greater<int>> pq) {
+    int cost = 0;
+    while (pq.size() > 1) {
+        int a = pq.top();
+        pq.pop();
+        int b = pq.top();
+        pq.pop();
+        int newrope = a + b;
+        cost += newrope;
+        pq.push(newrope);
+    }
+    return cost;
 }
 
+
+int main() {
+    priority_queue<int, vector<int>, greater<int>> pq;   //minHeap
+    int n, k;
+    cin >> n;
+    for (int j = 0; j < n; j++) {
+        cin >> k;
+        pq.push(k);
+    }
+    cout << joinTheRopes(pq);
+    return 0;
+}
