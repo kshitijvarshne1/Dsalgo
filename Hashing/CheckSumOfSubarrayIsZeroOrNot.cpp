824abba54321786ffecd707480f60faeeeb1f402
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-11 20:16:36 +0530

    add code to CheckSumOfSubarrayIsZeroOrNot.cpp

--- Hashing/CheckSumOfSubarrayIsZeroOrNot.cpp
+++ Hashing/CheckSumOfSubarrayIsZeroOrNot.cpp
@@ -21,7 +21,33 @@
 
 using namespace std;
 
+bool checkSumOfSubarrayIsZeroOrNot(int a[], int n) {
+    unordered_set<int> s;
+    int prefixSum = 0;
+    for (int i = 0; i < n; i++) {
+        prefixSum += a[i];
+        //check if sum is already 0 OR sum was present in the set
+        if (prefixSum == 0 or s.find(prefixSum) != s.end()) {
+            return true;
+        }
+        s.insert(prefixSum);
+    }
+    return false;
+}
+
 int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    if (checkSumOfSubarrayIsZeroOrNot(a, n)) {
+        cout << "Yes";
+    } else {
+        cout << "No";
+    }
+
     return 0;
 }
 
