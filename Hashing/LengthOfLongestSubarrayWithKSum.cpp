2fa2527945d60e6882407ad419cd23caafdbb0f4
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-11 22:12:34 +0530

    add code to LengthOfLongestSubarrayWithKSum.cpp

--- Hashing/LengthOfLongestSubarrayWithKSum.cpp
+++ Hashing/LengthOfLongestSubarrayWithKSum.cpp
@@ -20,8 +20,37 @@
 # include<unordered_set>
 
 using namespace std;
+int lengthOfLongestSubarrayKSum(int a[], int n, int k) {
+    unordered_map<int, int> m;
+    int prefixSum = 0;
+    int len = 0;
+    for (int i = 0; i < n; i++) {
+        prefixSum += a[i];
+        if (prefixSum == k) {
+            len = max(len, i + 1);
+        }
+        if (m.find(prefixSum - k) != m.end()) {
+            len = max(len, i - m[prefixSum - k]);
+        } else {
+            //store the first occurence
+            m[prefixSum] = i;
+        }
+    }
+    return len;
+}
 
 int main() {
+    int n;
+    cin >> n;
+    int k;
+    cin >> k;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    cout << lengthOfLongestSubarrayKSum(a, n, k);
+
+
     return 0;
 }
 
