cd8ab2044709ad37037e94c1580598ef6e3a331d
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-12 19:38:44 +0530

    add code to HighestFrequencyElement.cpp

--- Hashing/HighestFrequencyElement.cpp
+++ Hashing/HighestFrequencyElement.cpp
@@ -18,10 +18,28 @@
 # include<map>
 # include<unordered_map>
 # include<unordered_set>
-
 using namespace std;
-
 int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    unordered_map<int, int> m;
+    for (int i = 0; i < n; i++) {
+        m[a[i]]++;
+    }
+    int num = a[0];
+    int maxfre = 0;
+    for (int i = 0; i < n; i++) {
+        if (m.find(a[i])->second > maxfre) {
+            maxfre = m.find(a[i])->second;
+            num = m.find(a[i])->first;
+        }
+    }
+    cout << num;
+
+
     return 0;
 }
-
