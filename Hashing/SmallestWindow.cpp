9eb0139bcf4b6331c575cc4c60c1ce3063b1780d
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-12 14:28:48 +0530

    add code to SmallestWindow.cpp

--- Hashing/SmallestWindow.cpp
+++ Hashing/SmallestWindow.cpp
@@ -20,8 +20,64 @@
 # include<unordered_set>
 
 using namespace std;
+string find_window(string s, string p) {
+    int SL = s.length();
+    int PL = p.length();
+    //1.Corner case
+    if (PL > SL) {
+        return "None";
+    }
+    //2.Frequency map
+    int FS[256] = {0}; //frequency table of string s
+    int FP[256] = {0}; //frequency table of string p
 
-int main() {
-    return 0;
+    for (int i = 0; i < PL; i++) {
+        char ch = p[i];
+        FP[ch]++;
+    }
+    //3. Sliding window(Expansion & contraction )+ update the min length window
+    int cnt = 0;
+    int start = 0;
+    int min_length = INT_MAX;
+    int start_idx = -1;
+    for (int i = 0; i < SL; i++) {
+        char ch = s[i];
+        FS[ch]++;
+        //Maintain the cnt of character metched
+        if (FP[ch] != 0 and FS[ch] <= FP[ch]) {
+            cnt++;
+        }
+        //if all the charaters match
+        if (cnt == PL) {
+            // start shrinking the window
+            char temp = s[start];
+            //Loop to remove un-wanted characters
+            while (FP[temp] == 0 or FS[temp] > FP[temp]) {
+                FS[temp]--;
+                start++;
+                temp = s[start];
+            }
+            //Window Size
+            int window_length = i - start + 1;
+            if (window_length < min_length) {
+                min_length = window_length;
+                start_idx = start;
+            }
+        }
+    }
+    if (start_idx == -1) {
+        return "None";
+    }
+    string ans = s.substr(start_idx, min_length);
+    return ans;
 }
 
+int main() {
+    string s, p;
+    cin >> s;
+    cin >> p;
+    cout << find_window(s, p);
+
+
+    return 0;
+}
