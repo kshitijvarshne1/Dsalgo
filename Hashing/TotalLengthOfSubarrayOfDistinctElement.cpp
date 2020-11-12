26cd80a42b4c062282cc2e46aa7abc071282c453
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-12 22:09:26 +0530

    add code to TotalLengthOfSubarrayOfDistinctElement.cpp

--- Hashing/TotalLengthOfSubarrayOfDistinctElement.cpp
+++ Hashing/TotalLengthOfSubarrayOfDistinctElement.cpp
@@ -18,10 +18,29 @@
 # include<map>
 # include<unordered_map>
 # include<unordered_set>
-
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    unordered_set<int> m;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int ans = 0;
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        while (j < n and m.find(a[j]) == m.end()) {
+            m.insert(a[j]);
+            j++;
+        }
+        ans += ((j - i) * (j - i + 1)) / 2;
+        m.erase(a[i]);
+
+    }
+    cout << ans;
+
+
     return 0;
 }
-
