e8de015f55ddbd5b281e5e89d4adf0146679667e
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-12 19:29:19 +0530

    add code to Arrays-IntersectionOf TwoArrays.cpp

--- Hashing/Arrays-IntersectionOf TwoArrays.cpp
+++ Hashing/Arrays-IntersectionOf TwoArrays.cpp
@@ -22,6 +22,37 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    int a[n], b[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    unordered_map<int, int> m;
+    for (int i = 0; i < n; i++) {
+        m[a[i]]++;
+    }
+    int newArr[10000];
+    int c = 0;
+    for (int i = 0; i < n; i++) {
+        if (m.find(b[i]) != m.end()) {
+            if (m.find(b[i])->second > 0) {
+                newArr[c] = b[i];
+                c = c + 1;
+                int temp = m.find(b[i])->second;
+                temp -= 1;
+                m.erase(b[i]);
+                m[b[i]] = temp;
+            }
+        }
+    }
+    sort(newArr, newArr + c);
+    for (int i = 0; i < c; i++) {
+        cout << newArr[i] << " ";
+    }
+
     return 0;
 }
-
