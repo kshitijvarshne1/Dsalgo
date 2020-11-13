3ec3a6c3f9f8ad9536189a88634410bbcf45143a
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-13 23:33:30 +0530

    add code to EvenOdd.cpp

--- Basic/EvenOdd.cpp
+++ Basic/EvenOdd.cpp
@@ -20,8 +20,26 @@
 # include<unordered_set>
 
 using namespace std;
+// find odd and even using LSB bit of given number <- Reduces modulo complexity
+/*
+if n&1 ==1  <- odd
+if n&1 ==0  <- even
+ */
+/*
+if n=1 <- result is 1
+if n=0 <- result is 0
+*/
 
-int main() {
-    return 0;
+bool evenOdd(int n) {
+    return (n & 1);
 }
 
+int main() {
+    int n;
+    cin >> n;
+    if (evenOdd(n)) {
+        cout << "It is a odd number";
+    } else {
+        cout << "It is a even number";
+    }
+}
