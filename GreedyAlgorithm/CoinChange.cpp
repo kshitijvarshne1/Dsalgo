dc8a4d41fe6f2ade902e3c91538fc85b5c9bbb65
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-14 13:08:25 +0530

    add code to CoinChange.cpp

--- GreedyAlgorithm/CoinChange.cpp
+++ GreedyAlgorithm/CoinChange.cpp
@@ -20,8 +20,25 @@
 # include<unordered_set>
 
 using namespace std;
-
-int main() {
-    return 0;
+int make_change(int *coins, int n, int money) {
+    int ans = 0;
+    while (money > 0) {
+        int idx = upper_bound(coins, coins + n, money) - 1 - coins;
+        cout << coins[idx] << " ";
+        money = money - coins[idx];
+        ans++;
+    }
+    return ans;
 }
 
+int main() {
+    int money;
+    cin >> money;
+    int coins[] = {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
+    int t = sizeof(coins) / sizeof(int);
+    int totalCoins = make_change(coins, t, money);
+    cout << endl;
+    cout << "Total coins is " << totalCoins;
+
+    return 0;
+}
