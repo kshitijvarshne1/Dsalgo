fb036cb06a6a8a9ba5daa5e602d6ea6451dc0020
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-14 19:38:31 +0530

    add code to BiasedStanding.cpp

--- GreedyAlgorithm/BiasedStanding.cpp
+++ GreedyAlgorithm/BiasedStanding.cpp
@@ -18,10 +18,34 @@
 # include<map>
 # include<unordered_map>
 # include<unordered_set>
-
 using namespace std;
-
+//Baised standing
+//find badness of rank for the teams
 int main() {
+    int t;
+    cin >> t;
+    int arr[10000] = {0};
+    while (t--) {
+        memset(arr, 0, sizeof(arr));
+        int n;
+        cin >> n;
+        string name;
+        int rank;
+        for (int i = 0; i < n; i++) {
+            cin >> name >> rank;
+            arr[rank]++;
+        }
+        //Greedy Approach- assign the team nearest rank available
+        int actual_rank = 1, sum = 0;
+        for (int i = 1; i <= n; i++) {
+            while (arr[i]) {
+                sum += abs(actual_rank - i);
+                arr[i]--;
+                actual_rank++;
+            }
+        }
+        cout << sum << endl;
+    }
+
     return 0;
 }
-
