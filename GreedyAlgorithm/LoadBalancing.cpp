1f8b7ebaa876dcd70187b1cf1140a723265fca8c
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-15 00:55:24 +0530

    add code to LoadBalancing.cpp

--- GreedyAlgorithm/LoadBalancing.cpp
+++ GreedyAlgorithm/LoadBalancing.cpp
@@ -18,10 +18,54 @@
 # include<map>
 # include<unordered_map>
 # include<unordered_set>
-
+/*
+*   Created by CLion IDE.
+*   Author: Kshitij Varshney (kshitijvarshne1)
+*   Date: 14-Nov-20
+*   Time: 10:56 PM
+*   File: gg.cpp
+*/
+# include<iostream>
+# include<climits>
+# include<cmath>
+# include<algorithm>
+# include<bits/stdc++.h>
+# include<cstring>
+# include<iomanip>
+# include<vector>
+# include<stack>
+# include<queue>
+# include<map>
+# include<unordered_map>
+# include<unordered_set>
+//Defense of a kingdom
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    int width = 15, height = 8;
+    int x[n], y[n];
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+    //sorting
+    sort(x, x + n);
+    sort(y, y + n);
+//    int dx=0;
+//    int dy=0;
+    int dx = x[0];
+    int dy = y[0];
+    for (int i = 1; i < n; i++) {
+        dx = max(dx, x[i] - x[i - 1]);
+        dy = max(dy, y[i] - y[i - 1]);
+    }
+    //corner case
+    // dx=max(dx,width-x[n-1]);
+    // dy=max(dy,height-y[n-1]);
+    // cout<<dx*dy;
+    cout << (dx - 1) * (dy - 1);
+
     return 0;
 }
 
