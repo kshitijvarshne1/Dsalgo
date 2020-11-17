a5381d8d39dbae8e94f434ae0be3f71ec7696fd5
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-17 14:39:01 +0530

    add code to AdjacentImplementationFromGenericData.cpp

--- GraphAlgorithm/AdjacentImplementationFromGenericData.cpp
+++ GraphAlgorithm/AdjacentImplementationFromGenericData.cpp
@@ -18,10 +18,42 @@
 # include<map>
 # include<unordered_map>
 # include<unordered_set>
-
 using namespace std;
+class Graph {
+    //Adjacency list
+    unordered_map<string, list<pair<string, int>>> l;   //Hashing
+public:
+    void addEdge(string x, string y, bool bidir, int wt) {
+        l[x].push_back(make_pair(y, wt));
+        if (bidir) {
+            l[y].push_back(make_pair(x, wt));
+        }
+    }
 
+    void printAdjacencyList() {
+        //Iterate over all the keys in the map
+        for (auto p:l) {
+            string city = p.first;
+            list<pair<string, int>> nbrs = p.second;
+            cout << city << " -> ";
+            for (auto nbr : nbrs) {
+                string destination = nbr.first;
+                int distance = nbr.second;
+                cout << destination << " " << distance << " , ";
+            }
+            cout << endl;
+        }
+    }
+};
 int main() {
+    Graph g;
+    g.addEdge("A", "B", true, 20);
+    g.addEdge("B", "D", true, 40);
+    g.addEdge("A", "C", true, 10);
+    g.addEdge("C", "D", true, 40);
+    g.addEdge("A", "D", false, 50);
+    g.printAdjacencyList();
+
     return 0;
 }
 
