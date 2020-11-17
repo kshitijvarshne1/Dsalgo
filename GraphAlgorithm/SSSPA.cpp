777ed4a7c4451f8b3388af086ee447a9ad31afea
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-17 15:18:18 +0530

    add code to SSSPA.cpp

--- GraphAlgorithm/SSSPA.cpp
+++ GraphAlgorithm/SSSPA.cpp
@@ -20,8 +20,56 @@
 # include<unordered_set>
 
 using namespace std;
+template<typename T>
+class Graph {
+    map<T, list<T>> l;
+public:
+    void addEdge(T x, T y) {
+        l[x].push_back(y);
+        l[y].push_back(x);
+    }
 
+    void SSSPA(T src) {
+        map<T, int> dist;
+        queue<T> q;
+        //All other node will have INT_MAX
+        for (auto node_pair:l) {
+            T node = node_pair.first;
+            dist[node] = INT_MAX;
+        }
+        q.push(src);
+        dist[src] = 0;
+
+        while (!q.empty()) {
+            T node = q.front();
+            q.pop();
+
+            for (int nbr : l[node]) {
+                if (dist[nbr] == INT_MAX) {
+                    q.push(nbr);
+                    dist[nbr] = dist[node] + 1;
+                }
+            }
+        }
+        //print the dist to every node
+        for (auto node_pair:l) {
+            T node = node_pair.first;
+            int d = dist[node];
+            cout << "Node " << node << " Dist from src " << d << endl;
+        }
+    }
+};
 int main() {
+    Graph<int> g;
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    g.addEdge(3, 0);
+    g.addEdge(3, 4);
+    g.addEdge(4, 5);
+    g.SSSPA(0);
+
     return 0;
 }
+
 
