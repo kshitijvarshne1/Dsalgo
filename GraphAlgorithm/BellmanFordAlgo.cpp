e9da39c0c64ed0422c4c87fc6cdceb37d3210e09
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-21 00:14:49 +0530

    add code to BellmanFordAlgo.cpp

--- GraphAlgorithm/BellmanFordAlgo.cpp
+++ GraphAlgorithm/BellmanFordAlgo.cpp
@@ -21,7 +21,51 @@
 
 using namespace std;
 
-int main() {
-    return 0;
+vector<int> bellmanFordAlgo(int v, int src, vector<vector<int>> edges) {
+    //create a vector
+    vector<int> distance(v + 1, INT_MAX);
+    distance[src] = 0;
+
+    for (int i = 0; i < v - 1; i++) {
+        for (auto edge:edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+            if (distance[u] != INT_MAX and distance[u] + wt < distance[v]) {
+                distance[v] = distance[u] + wt;
+            }
+        }
+    }
+
+    //negative weight cycle
+    for (auto edge:edges) {
+        int u = edge[0];
+        int v = edge[1];
+        int wt = edge[2];
+        if (distance[u] != INT_MAX and distance[u] + wt < distance[v]) {
+            cout << "negative cycle found ";
+            exit(0);
+        }
+    }
+    return distance;
 }
 
+
+int main() {
+    int v, e;
+    cin >> v >> e;
+    //edge list
+    vector<vector<int>> edges;
+
+    for (int i = 0; i < e; i++) {
+        int u, v, wt;
+        cin >> u >> v >> wt;
+        edges.push_back({u, v, wt});
+    }
+    vector<int> distance = bellmanFordAlgo(v, 1, edges);
+    for (int i = 1; i <= v; i++) {
+        cout << "Node " << i << " is at a distance " << distance[i] << endl;
+    }
+
+    return 0;
+}
