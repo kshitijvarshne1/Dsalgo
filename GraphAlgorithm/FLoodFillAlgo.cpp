7de027c5a60cf76a88a04c89e10b8ce5de81b272
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-20 22:54:14 +0530

    add code to FLoodFillAlgo.cpp

--- GraphAlgorithm/FLoodFillAlgo.cpp
+++ GraphAlgorithm/FLoodFillAlgo.cpp
@@ -20,8 +20,44 @@
 # include<unordered_set>
 
 using namespace std;
+int R, C;
+int dx[] = {-1, 0, 1, 0}; //{west,north,east,south}
+int dy[] = {0, -1, 0, 1};
+
+void floodFill(char mat[][50], int i, int j, char ch, char color) {
+    //Base case ->Matrix bounds
+    if (i <= 0 || j < 0 || i >= R || j >= C) {
+        return;
+    }
+    //Figure Boundary condition
+    if (mat[i][j] != ch) {
+        return;
+    }
+    //Recursive boundary condition
+    mat[i][j] = color;
+    for (int k = 0; i < 4; k++) {
+        floodFill(mat, i + dx[k], j + dy[k], ch, color);
+    }
+}
 
 int main() {
+
+
+    cin >> R >> C;
+    char input[15][50];
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cin >> input[i][j];
+        }
+    }
+    floodFill(input, 8, 13, '.', '&');
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cout << input[i][j];
+        }
+        cout << endl;
+    }
+
     return 0;
 }
 
