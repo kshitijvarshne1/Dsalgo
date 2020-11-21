49011cffeb250d9673ed24cd9bba23e43b6bda26
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-22 02:15:46 +0530

    add code to CoinChangeProblemUsingBottomUp.cpp

--- DynamicProgramming/CoinChangeProblem/CoinChangeProblemUsingBottomUp.cpp
+++ DynamicProgramming/CoinChangeProblem/CoinChangeProblemUsingBottomUp.cpp
@@ -1,4 +1,4 @@
-r/*
+/*
 *   Created by CLion IDE.
 *   Author: Kshitij Varshney (kshitijvarshne1)
 *   Date: 22-Nov-20
@@ -21,7 +21,26 @@ r/*
 
 using namespace std;
 
+int minCoins(int Num, int coins[], int t) {
+    int dp[1000] = {0};
+
+    for (int n = 1; n <= Num; n++) {
+        dp[n] = INT_MAX;
+        for (int i = 0; i < t; i++) {
+            if (n - coins[i] >= 0) {
+                int subProblem = dp[n - coins[i]];
+                dp[n] = min(dp[n], subProblem + 1);
+            }
+        }
+    }
+    return dp[Num];
+}
 int main() {
+    int n;
+    cin >> n;
+    int coins[] = {1, 7, 10};
+    int t = sizeof(coins) / sizeof(int);
+    cout << minCoins(n, coins, t);
+
     return 0;
 }
-
