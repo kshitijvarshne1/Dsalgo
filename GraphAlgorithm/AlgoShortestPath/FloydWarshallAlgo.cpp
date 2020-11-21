16be5e6befe5baf880cae303b5867691e788de85
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-21 11:18:59 +0530

    add code to FloydWarshallAlgo.cpp

--- GraphAlgorithm/AlgoShortestPath/FloydWarshallAlgo.cpp
+++ GraphAlgorithm/AlgoShortestPath/FloydWarshallAlgo.cpp
@@ -18,10 +18,41 @@
 # include<map>
 # include<unordered_map>
 # include<unordered_set>
-
+# define INF 1000
 using namespace std;
 
+vector<vector<int>> floydWarshall(vector<vector<int>> graph) {
+    vector<vector<int>> distance(graph);
+    int v = graph.size();
+    //phase , in kth phase we included vertices (1,2...k) as intermediate vertices
+    for (int k = 0; k < v; k++) {
+        //Iterate over the entire 2D vertices matrix
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < v; j++) {
+                //vertex k is included and we can minimise the distance
+                if (distance[i][j] > distance[i][k] + distance[k][j]) {
+                    distance[i][j] = distance[i][k] + distance[k][j];
+                }
+            }
+        }
+    }
+    return distance;
+}
+
 int main() {
-    return 0;
+    //4 vertices (4*4) matix
+    vector<vector<int>> graph = {
+            {0,   INF, -2, INF},
+            {4,   0,   3,  INF},
+            {INF, INF, 0,  2},
+            {INF, -1, INF, 0}
+    };
+    auto result = floydWarshall(graph);
+    for (int i = 0; i < result.size(); i++) {
+        for (int j = 0; j < result.size(); j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
 
