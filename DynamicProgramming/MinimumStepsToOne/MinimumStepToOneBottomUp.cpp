b6949582a51b8426f009ecbc41c8aa788dd75fea
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-22 01:01:37 +0530

    add code to MinimumStepToOneBottomUp.cpp

--- DynamicProgramming/MinimumStepsToOne/MinimumStepToOneBottomUp.cpp
+++ DynamicProgramming/MinimumStepsToOne/MinimumStepToOneBottomUp.cpp
@@ -20,8 +20,33 @@
 # include<unordered_set>
 
 using namespace std;
-
-int main() {
-    return 0;
+// find minimum steps to 1
+//n->n/2
+//n->n/3
+//n=n-1
+//Down up approach
+int minSteps(int n) {
+    int dp[1000] = {0};
+    dp[1] = 0;
+    for (int i = 2; i <= n; i++) {
+        int op1, op2, op3;
+        op1 = op2 = op3 = INT_MAX;
+        if (n % 3 == 0) {
+            op1 = dp[i / 3];
+        }
+        if (n % 2 == 0) {
+            op2 = dp[i / 2];
+        }
+        op3 = dp[i - 1];
+        dp[i] = min(min(op1, op2), op3) + 1;
+    }
+    return dp[n];
 }
 
+int main() {
+    int n;
+    cin >> n;
+    cout << minSteps(n);
+
+    return 0;
+}
