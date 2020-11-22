f3cd6ade4ec438bf6d427c0ecebf3d0ba8039db4
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-22 15:14:54 +0530

    add code to MaximumSubarraySum.cpp

--- DynamicProgramming/MaximumSubarraySum.cpp
+++ DynamicProgramming/MaximumSubarraySum.cpp
@@ -20,8 +20,24 @@
 # include<unordered_set>
 
 using namespace std;
-
-int main() {
-    return 0;
+int maxSum(int a[], int n) {
+    int dp[100] = {0};
+    dp[0] = a[0] > 0 ? a[0] : 0;
+    int maxSoFar = a[0];
+    for (int i = 1; i < n; i++) {
+        dp[i] = dp[i - 1] + a[i];
+        if (dp[i] < 0) {
+            dp[i] = 0;
+        }
+        maxSoFar = max(dp[i], maxSoFar);
+    }
+    return maxSoFar;
 }
 
+int main() {
+    int a[] = {-3, 2, 5, -1, 6, 3, -2, 7, -5, 2};
+    int n = sizeof(a) / sizeof(int);
+    cout << maxSum(a, n);
+
+    return 0;
+}
