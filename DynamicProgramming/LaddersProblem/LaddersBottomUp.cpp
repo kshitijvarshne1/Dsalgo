5a5749872e1c92e0a755c62343ac4f68222fcb2f
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-22 18:17:47 +0530

    add code to LaddersBottomUp.cpp

--- DynamicProgramming/LaddersProblem/LaddersBottomUp.cpp
+++ DynamicProgramming/LaddersProblem/LaddersBottomUp.cpp
@@ -19,9 +19,24 @@
 # include<unordered_map>
 # include<unordered_set>
 
-using namespace std;
-
-int main() {
-    return 0;
+int ladders_bottomUp(int n, int k) {
+    int dp[100] = {0};
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        dp[i] = 0;
+        for (int j = 1; j <= k; j++) {
+            if (i - j >= 0) {
+                dp[i] = dp[i] + dp[i - j];
+            }
+        }
+    }
+    return dp[n];
 }
 
+int main() {
+    int n, k;
+    cin >> n >> k;
+    cout << ladders_bottomUp(n, k);
+
+    return 0;
+}
