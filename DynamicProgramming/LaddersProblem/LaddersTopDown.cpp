d1790571fa80f9d0ba70055e463dd6a425a47cba
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-22 17:27:51 +0530

    add code to LaddersTopDown.cpp

--- DynamicProgramming/LaddersProblem/LaddersTopDown.cpp
+++ DynamicProgramming/LaddersProblem/LaddersTopDown.cpp
@@ -21,7 +21,27 @@
 
 using namespace std;
 
-int main() {
-    return 0;
+int ladders_topdown(int n, int k, int dp[]) {
+    if (n == 0) {
+        return 1;
+    }
+    if (dp[n] != 0) {
+        return dp[n];
+    }
+    int ways = 0;
+    for (int i = 1; i <= k; i++) {
+        if (n - i >= 0) {
+            ways = ways + ladders_topdown(n - i, k, dp);
+        }
+    }
+    return dp[n] = ways;
 }
 
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int dp[1000] = {0};
+    cout << ladders_topdown(n, k, dp);
+
+    return 0;
+}
