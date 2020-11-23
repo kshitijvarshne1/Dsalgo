bc17e39df7abc51c7c65475e7a4f087631f8cbcd
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-23 21:37:27 +0530

    add code to LCSUsingRecursion.cpp

--- DynamicProgramming/LCS/LCSUsingRecursion.cpp
+++ DynamicProgramming/LCS/LCSUsingRecursion.cpp
@@ -21,7 +21,23 @@
 
 using namespace std;
 
-int main() {
-    return 0;
+int lcs(string s1, string s2, int i, int j) {
+    if (i == s1.length() or j == s2.length()) {
+        return 0;
+    }
+    if (s1[i] == s2[j]) {
+        return 1 + lcs(s1, s2, i + 1, j + 1);
+    }
+    int op1 = lcs(s1, s2, i, j + 1);
+    int op2 = lcs(s1, s2, i + 1, j);
+    return max(op1, op2);
 }
 
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << lcs(s1, s2, 0, 0);
+
+    return 0;
+}
