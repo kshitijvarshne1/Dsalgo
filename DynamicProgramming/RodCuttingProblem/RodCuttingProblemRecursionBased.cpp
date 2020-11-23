8681f513754734bc4b81eec51a54769ffdb1be91
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-23 19:36:48 +0530

    add code to RodCuttingProblemRecursionBased.cpp

--- DynamicProgramming/RodCuttingProblem/RodCuttingProblemRecursionBased.cpp
+++ DynamicProgramming/RodCuttingProblem/RodCuttingProblemRecursionBased.cpp
@@ -22,7 +22,23 @@
 
 using namespace std;
 
-int main() {
-    return 0;
+int maxProfit(int prices[], int n) {
+    if (n <= 0) {
+        return 0;
+    }
+    int ans = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        int cut = i + 1;
+        int currentAns = prices[i] + maxProfit(prices, n - cut);
+        ans = max(currentAns, ans);
+    }
+    return ans;
 }
 
+int main() {
+    int prices[] = {1, 5, 8, 9, 10, 17, 17, 20};
+    int n = sizeof(prices) / sizeof(int);
+    cout << maxProfit(prices, n);
+
+    return 0;
+}
