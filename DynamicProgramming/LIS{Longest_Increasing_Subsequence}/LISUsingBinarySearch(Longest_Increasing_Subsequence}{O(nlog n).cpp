3c3e5309925c9eaa9bb836d737b6992cdd385daa
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-24 13:52:24 +0530

    add code to LISUsingBinarySearch(Longest_Increasing_Subsequence}{O(nlog n).cpp

--- DynamicProgramming/LIS{Longest_Increasing_Subsequence}/LISUsingBinarySearch(Longest_Increasing_Subsequence}{O(nlog n).cpp
+++ DynamicProgramming/LIS{Longest_Increasing_Subsequence}/LISUsingBinarySearch(Longest_Increasing_Subsequence}{O(nlog n).cpp
@@ -3,7 +3,7 @@
 *   Author: Kshitij Varshney (kshitijvarshne1)
 *   Date: 24-Nov-20
 *   Time: 1:51 PM
-*   File: LISUsingBinarySearch.cpp
+*   File: LISUsingBinarySearch(Longest_Increasing_Subsequence}{O(nlog n).cpp
 */
 # include<iostream>
 # include<bits/stdc++.h>
@@ -22,6 +22,33 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int dp[n + 1];
+    dp[0] = INT_MIN;
+
+    for (int i = 1; i <= n; i++) {
+        dp[i] = INT_MAX;
+    }
+
+    for (int i = 0; i <= n; i++) {
+        int length = upper_bound(dp, dp + n + 1, a[i]) - dp;
+        if (dp[length - 1] < a[i] && dp[length]) {
+            dp[length] = a[i];
+        }
+    }
+    int lis = 0;
+    for (int i = 1; i <= n; i++) {
+        if (dp[i] != INT_MAX) {
+            lis = i;
+        }
+    }
+    cout << lis;
+
+
     return 0;
 }
-
