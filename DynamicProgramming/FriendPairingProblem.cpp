1a152709679f067a618f1f6c73df1de5d56f39f9
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-24 20:35:18 +0530

    add code to FriendPairingProblem.cpp

--- DynamicProgramming/FriendPairingProblem.cpp
+++ DynamicProgramming/FriendPairingProblem.cpp
@@ -21,7 +21,22 @@
 
 using namespace std;
 
-int main() {
-    return 0;
+int countFriendsPairings(int n) {
+    int dp[n + 1];
+    for (int i = 0; i <= n; i++) {
+        if (i <= 2)
+            dp[i] = i;
+        else
+            dp[i] = dp[i - 1] + (i - 1) * dp[i - 2];
+    }
+    return dp[n];
 }
 
+
+int main() {
+    int n;
+    cin >> n;
+    cout << countFriendsPairings(n);
+
+    return 0;
+}
