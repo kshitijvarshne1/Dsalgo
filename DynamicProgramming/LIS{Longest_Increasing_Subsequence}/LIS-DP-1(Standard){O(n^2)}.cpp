bcb07ba728dd528035b9ed1de162760b1baf9434
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-24 12:07:05 +0530

    add code to LIS-DP-1(Standard){O(n^2)}.cpp

--- DynamicProgramming/LIS{Longest_Increasing_Subsequence}/LIS-DP-1(Standard){O(n^2)}.cpp
+++ DynamicProgramming/LIS{Longest_Increasing_Subsequence}/LIS-DP-1(Standard){O(n^2)}.cpp
@@ -3,7 +3,7 @@
 *   Author: Kshitij Varshney (kshitijvarshne1)
 *   Date: 24-Nov-20
 *   Time: 12:05 PM
-*   File: LIS.cpp
+*   File: LIS-DP-1(Standard){O(n^2)}.cpp
 */
 # include<iostream>
 # include<bits/stdc++.h>
@@ -22,6 +22,26 @@
 using namespace std;
 
 int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    int dp[n];
+    for (int i = 0; i < n; i++) {
+        dp[i] = 1;
+        for (int j = 0; j < i; j++) {
+            if (a[j] < a[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+    int lis = 0;
+    for (int i = 0; i < n; i++) {
+        lis = max(lis, dp[i]);
+    }
+    cout << lis;
+
     return 0;
 }
-
