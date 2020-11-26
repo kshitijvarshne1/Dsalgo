7bf8418e237ee633ab2c3bec7fe2c047764874b8
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-26 17:24:16 +0530

    add code to StringTrieInsertAndSearch.cpp

--- StringTrie/StringTrieInsertAndSearch.cpp
+++ StringTrie/StringTrieInsertAndSearch.cpp
@@ -22,10 +22,57 @@
 # define mod 10000000007
 using namespace std;
 
+struct trie {
+    trie *nxt[26];
+    bool ended;
+
+    trie() {
+        for (int i = 0; i < 26; i++) {
+            nxt[i] = NULL;
+        }
+        ended = false;
+    }
+};
+
+trie *root;
+
+void insert(string s) {
+    trie *current = root;
+    for (int i = 0; i < s.size(); i++) {
+        if (current->nxt[s[i] - 'a'] == NULL) {
+            current->nxt[s[i] - 'a'] = new trie();
+        }
+        current = current->nxt[s[i] - 'a'];
+    }
+    current->ended = true;
+}
+
+bool in_trie(string s) {
+    trie *current = root;
+    for (int i = 0; i < s.size(); i++) {
+        if (current->nxt[s[i] - 'a'] == NULL) {
+            return false;
+        }
+        current = current->nxt[s[i] - 'a'];
+    }
+    return current->ended;
+}
+
+
 int main() {
     /*ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
     */
+    root = new trie();
+    insert("apar");
+    insert("aryan");
+    insert("shivam");
+
+    if (in_trie("aapar")) {
+        cout << "Yes found";
+    } else {
+        cout << "Not found";
+    }
 
     return 0;
 }
