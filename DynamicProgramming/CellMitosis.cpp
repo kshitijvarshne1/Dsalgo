dd972b8cd0ab69f93ecf16af25c53fd775e9532d
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-26 12:31:39 +0530

    add code to CellMitosis.cpp

--- DynamicProgramming/CellMitosis.cpp
+++ DynamicProgramming/CellMitosis.cpp
@@ -22,7 +22,23 @@
 # define mod 10000000007
 using namespace std;
 
-int main() {
-    return 0;
+int solveCellProblem(int n, int x, int y, int z) {
+    int *dp = new int[n + 1];
+    dp[0] = 0;
+    dp[1] = 0;
+    for (int i = 2; i <= n; i++) {
+        if (i % 2 == 0) {
+            dp[i] = min(dp[i / 2] = x, dp[i - 1] + y);
+        } else {
+            dp[i] = min(dp[i - 2] + y, dp[(i + 1) / 2] + x + z);
+        }
+    }
+    return dp[n];
 }
 
+int main() {
+    int n, x, y, z;
+    cin >> n >> x >> y >> z;
+    cout << solveCellProblem(n, x, y, z);
+
+}
