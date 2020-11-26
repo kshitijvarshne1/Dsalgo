545ce5da21080e927eea9733d2cb476cf579141c
kshitij varshney <kshitijvarshney92@gmail.com>
2020-11-26 21:57:39 +0530

    add code to StringHashing.cpp

--- StringTrie/StringHashing.cpp
+++ StringTrie/StringHashing.cpp
@@ -19,14 +19,30 @@
 # include<unordered_map>
 # include<unordered_set>
 
-# define mod 10000000007
+#define ll long long int
+const ll mod = 1e9 + 7;
+const ll p = 31;
 using namespace std;
+
+ll poly_hash_string(string s) {
+    ll p_power = 1;
+    ll hash = 0;
+    for (ll i = 0; i < s.size(); i++) {
+        hash += (p_power * (s[i] - 'a' + 1));
+        p_power *= p;
+        p_power %= mod;
+        hash %= mod;
+    }
+    return hash;
+}
 
 int main() {
     /*ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
     */
 
+    string text = "abababab", pat = "aba";
+    cout << poly_hash_string(text);
+
     return 0;
 }
-
