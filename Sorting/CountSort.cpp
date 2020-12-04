28096d615f65ad0b0c4e63cbe91e3bfb602bd672
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-05 00:27:34 +0530

    add code to CountSort.cpp

--- Sorting/CountSort.cpp
+++ Sorting/CountSort.cpp
@@ -23,11 +23,40 @@
 # define ll long long int
 using namespace std;
 
+void countSort(int a[], int n) {
+    int k = a[0];
+    for (int i = 0; i < n; ++i) {
+        k = max(k, a[i]);
+    }
+
+    int count[10000] = {0};
+    for (int i = 0; i < n; ++i) {
+        count[a[i]]++;
+    }
+    for (int i = 0; i <= k; ++i) {
+        count[i] += count[i - 1];
+    }
+    int output[10000];
+    for (int i = n - 1; i >= 0; i--) {
+        output[--count[a[i]]] = a[i];
+    }
+    for (int i = 0; i < n; ++i) {
+        cout << output[i] << " ";
+    }
+
+}
+
 int main() {
     /*ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
     */
-
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    countSort(a, n);
     return 0;
 }
 
