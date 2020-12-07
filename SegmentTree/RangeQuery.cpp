6d5f348abfe96c524504d9723ff474e0468e6671
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-07 22:23:20 +0530

    add code to RangeQuery.cpp

--- SegmentTree/RangeQuery.cpp
+++ SegmentTree/RangeQuery.cpp
@@ -23,11 +23,46 @@
 # define ll long long int
 using namespace std;
 
+int CST(int segmentStart, int segmentEnd, int treeIndex, int arr[], int tree[]) {
+    if (segmentStart == segmentEnd) {
+        tree[treeIndex] = arr[segmentStart];
+        return arr[segmentStart];
+    }
+
+    int mid = (segmentStart + segmentEnd) / 2;
+
+    tree[treeIndex] = CST(segmentStart, mid, 2 * treeIndex + 1, arr, tree)
+                      + CST(mid + 1, segmentEnd, 2 * treeIndex + 2, arr, tree);
+
+    return tree[treeIndex];
+}
+
+int getSumRec(int queryStart, int queryEnd, int segmentStart, int segmentEnd, int treeIndex, int tree[]) {
+    if (segmentEnd < queryStart || segmentStart > queryEnd)
+        return 0;
+    if (queryStart <= segmentStart && queryEnd >= segmentEnd)
+        return tree[treeIndex];
+
+    int mid = (segmentStart + segmentEnd) / 2;
+
+    return getSumRec(queryStart, queryEnd, segmentStart, mid, 2 * treeIndex + 1, tree)
+           + getSumRec(queryStart, queryEnd, mid + 1, segmentEnd, 2 * treeIndex + 2, tree);
+}
+
+
 int main() {
     /*ios_base::sync_with_stdio(false);
-    cin.tie(NULL);cout.tie(NULL);
-    */
+   cin.tie(NULL);cout.tie(NULL);
+   */
+
+    int arr[] = {10, 20, 30, 40}, n = 4;
+
+    int tree[4 * n];
+    CST(0, n - 1, 0, arr, tree);
+
+    cout << getSumRec(0, 2, 0, 3, 0, tree);
 
     return 0;
 }
+
 
