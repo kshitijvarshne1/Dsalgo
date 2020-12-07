775e0eb639680af59f39adf238eb05221efeed65
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-07 23:12:03 +0530

    add code to UpdateQuery.cpp

--- SegmentTree/UpdateQuery.cpp
+++ SegmentTree/UpdateQuery.cpp
@@ -23,11 +23,45 @@
 # define ll long long int
 using namespace std;
 
+int CST(int segmentStart, int segmentEnd, int treeIndex, int arr[], int tree[]) {
+    if (segmentStart == segmentEnd) {
+        tree[treeIndex] = arr[segmentStart];
+        return arr[segmentStart];
+    }
+
+    int mid = (segmentStart + segmentEnd) / 2;
+
+    tree[treeIndex] = CST(segmentStart, mid, 2 * treeIndex + 1, arr, tree)
+                      + CST(mid + 1, segmentEnd, 2 * treeIndex + 2, arr, tree);
+
+    return tree[treeIndex];
+}
+
+void updateRec(int segmentStart, int segmentEnd, int i, int treeIndex, int diff, int tree[]) {
+    if (i < segmentStart || i > segmentEnd)
+        return;
+
+    tree[treeIndex] = tree[treeIndex] + diff;
+
+    if (segmentEnd > segmentStart) {
+        int mid = (segmentStart + segmentEnd) / 2;
+
+        updateRec(segmentStart, mid, i, 2 * treeIndex + 1, diff, tree);
+        updateRec(mid + 1, segmentEnd, i, 2 * treeIndex + 2, diff, tree);
+
+    }
+}
+
 int main() {
     /*ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
     */
+    int arr[] = {10, 20, 30, 40}, n = 4;
+
+    int tree[4 * n];
+    CST(0, n - 1, 0, arr, tree);
+
+    updateRec(0, 3, 1, 0, 5, tree);
 
     return 0;
 }
-
