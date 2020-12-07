a9150797a4a7d37edfefeed4d21f11b888ed5e2c
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-07 21:53:33 +0530

    create BuiltSegmentTree.cpp

---
+++ SegmentTree/BuiltSegmentTree.cpp
@@ -0,0 +1,33 @@
+/*  
+*   Created by CLion IDE.
+*   Author: Kshitij Varshney (kshitijvarshne1)
+*   Date: 07-Dec-20
+*   Time: 9:53 PM
+*   File: BuiltSegmentTree.cpp
+*/
+# include<iostream>
+# include<bits/stdc++.h>
+# include<climits>
+# include<cmath>
+# include<algorithm>
+# include<cstring>
+# include<iomanip>
+# include<vector>
+# include<stack>
+# include<queue>
+# include<map>
+# include<unordered_map>
+# include<unordered_set>
+
+# define modValue 1e9 + 7
+# define ll long long int
+using namespace std;
+
+int main() {
+    /*ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    */
+
+    return 0;
+}
+
