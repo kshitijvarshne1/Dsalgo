f6fe9f121c81181a063b50f83071ab0e1145f6f2
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-07 21:54:28 +0530

    add code to BuiltSegmentTree.cpp

--- SegmentTree/BuiltSegmentTree.cpp
+++ SegmentTree/BuiltSegmentTree.cpp
@@ -23,11 +23,28 @@
 # define ll long long int
 using namespace std;
 
+int constructSegmentTree(int segmentStart, int segmentEnd, int treeIndex, int arr[], int tree[]) {
+    if (segmentStart == segmentEnd) {
+        tree[treeIndex] = arr[segmentStart];
+        return arr[segmentStart];
+    }
+
+    int mid = (segmentStart + segmentEnd) / 2;
+
+    tree[treeIndex] = constructSegmentTree(segmentStart, mid, 2 * treeIndex + 1, arr, tree)
+                      + constructSegmentTree(mid + 1, segmentEnd, 2 * treeIndex + 2, arr, tree);
+
+    return tree[treeIndex];
+}
+
 int main() {
     /*ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
     */
+    int arr[] = {10, 20, 30, 40}, n = 4;
+
+    int tree[4 * n];
+    cout << constructSegmentTree(0, n - 1, 0, arr, tree) << endl;
 
     return 0;
 }
-
