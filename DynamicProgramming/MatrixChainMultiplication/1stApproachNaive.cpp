ffb17e3277ae6d33d1ef55f7313d148a7c699cb0
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-11 22:58:24 +0530

    add code to 1stApproachNaive.cpp

--- DynamicProgramming/MatrixChainMultiplication/1stApproachNaive.cpp
+++ DynamicProgramming/MatrixChainMultiplication/1stApproachNaive.cpp
@@ -23,11 +23,33 @@
 # define ll long long int
 using namespace std;
 
+
+int MatrixChainOrder(int p[], int i, int j) {
+    if (i == j) {
+        return 0;
+    }
+    int k;
+    int min = INT_MAX;
+    int count;
+
+    for (k = i; k < j; k++) {
+        count = MatrixChainOrder(p, i, k) + MatrixChainOrder(p, k + 1, j) + p[i - 1] * p[k] * p[j];
+
+        if (count < min)
+            min = count;
+    }
+
+    return min;
+}
+
 int main() {
     /*ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
     */
+    int arr[] = {40, 20, 30, 10, 30};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Minimum number of multiplications is " << MatrixChainOrder(arr, 1, n - 1);
 
     return 0;
 }
-
