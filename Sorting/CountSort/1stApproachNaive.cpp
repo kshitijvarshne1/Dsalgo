144d0ef1d67b765c86047c8d16153cf222fd010b
kshitij varshney <kshitijvarshney92@gmail.com>
2020-12-13 00:39:39 +0530

    final commit

--- Sorting/CountSort/1stApproachNaive.cpp
+++ Sorting/CountSort/1stApproachNaive.cpp
@@ -40,7 +40,7 @@ void countSort(int arr[], int n, int k) {
     }
     for (int i = 0; i < n; i++)
         arr[i] = output[i];
-
+    return;
 }
 
 int main() {
@@ -51,9 +51,7 @@ int main() {
     int n = sizeof(arr) / sizeof(arr[0]);
     int k = 5;
     countSort(arr, n, k);
-
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
-
     return 0;
 }
